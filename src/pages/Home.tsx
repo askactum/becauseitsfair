dca2a74cf024351bec8261b3302c82a9b2516836
Surya Singh <ssurya5599@gmail.com>
2025-04-19 23:35:52 -0700

    updates to the app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,12 @@
 export default function Home() {
   return (
     <main className="main-content">
-      <div className="home-content">
-        <div className="semicircle extend-right">
-          <div className="semicircle-content">
-            <div className="main-title">a nonprofit to create free, universal housing.</div>
-            <div className="because-box">
-              <div>because<br/>it's<br/>fair</div>
-              <div className="dot"></div>
-            </div>
-          </div>
+      <div className="home-content" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '60vh' }}>
+        <div className="main-title" style={{ fontSize: '2.2rem', fontWeight: 500, marginBottom: '2.2rem', textAlign: 'center' }}>
+          a nonprofit to create free, universal housing.
+        </div>
+        <div className="because-box" style={{ fontSize: '2.1rem', fontWeight: 700, textAlign: 'center', lineHeight: 1.1, color: '#222' }}>
+          <div>because<br/>it's<br/>fair</div>
         </div>
       </div>
     </main>
